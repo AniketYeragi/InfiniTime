d3870a7f2e9bd91a49e77d8d703eae3116ce00fc
Aniket Yeragi <aniket.yeragi@nc-systems.de>
2021-05-27 12:08:31 +0200

    Long press activation

--- src/displayapp/screens/FallDetection.cpp
+++ src/displayapp/screens/FallDetection.cpp
@@ -10,7 +10,7 @@ FallDetection::FallDetection(Pinetime::Applications::DisplayApp* app, Pinetime::
   imgFlag = lv_label_create(lv_scr_act(), nullptr);
   lv_obj_set_style_local_text_font(imgFlag, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &lv_font_navi_80);
   lv_obj_set_style_local_text_color(imgFlag, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_CYAN);
-  lv_label_set_text(imgFlag, iconForName("flag"));
+  lv_label_set_text(imgFlag, "!");
   lv_obj_align(imgFlag, nullptr, LV_ALIGN_CENTER, 0, -60);
 }
 
